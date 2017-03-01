3cbc14a2baf5f48144deb8c73daed165a878f087
steve <stvalett@student.42.fr>
2017-03-01 10:14:45 +0000

    Finish segfault

--- src/command.c
+++ src/command.c
@@ -14,96 +14,95 @@
 
 int	ft_cmd_echo(char **av, char **env_bis)
 {
-	int 	i;
-	int		flag;
+  int 	i;
+  int		flag;
 
-	flag = 0;
-	if (av[0] != NULL)
+  flag = 0;
+  if (av[0] != NULL)
+  {
+    if (av[1] != NULL)
+    {
+      ft_check_dollar_n_acco(av, &flag);
+      if (flag == 1)
+	ft_print_dollar(av, env_bis);
+      else if (flag == 2)
+	;
+      //ft_print_dollar_n_acco(av, env_bis);
+      else if (flag == 3)
+	ft_print_acco(av);
+      else
+      {
+	i = 0;
+	while (av[++i])
 	{
-		if (av[1] != NULL)
-		{
-			ft_check_dollar_n_acco(av, &flag);
-			if (flag == 1)
-				ft_print_dollar(av, env_bis);
-			else if (flag == 2)
-				;
-				//ft_print_dollar_n_acco(av, env_bis);
-			else if (flag == 3)
-				;
-				//ft_print_acco(av);
-			else
-			{
-				i = 0;
-				while (av[++i])
-				{
-					ft_putstr(av[i]);
-					ft_putchar(' ');
-				}
-				ft_putchar('\n');
-			}
-		}
+	  ft_putstr(av[i]);
+	  ft_putchar(' ');
 	}
-	return (1);
+	ft_putchar('\n');
+      }
+    }
+  }
+  return (1);
 }
 
 int	ft_cmd_unsetenv(char **av, char ***env)
 {
-	if (av[1] != NULL)
-	{
-		if (av[2] != NULL)
-			*env = ft_unsetenv(av[1], av[2], *env);
-		else
-			*env = ft_unsetenv(av[1], "", *env);
-	}
-	else
-		ft_putendl_fd("Error too few argument", 2);
-	return (1);
+  if (av[1] != NULL)
+  {
+    if (av[2] != NULL)
+      *env = ft_unsetenv(av[1], av[2], *env);
+    else
+      *env = ft_unsetenv(av[1], "", *env);
+  }
+  else
+    ft_putendl_fd("Error too few argument", 2);
+  return (1);
 }
 
 int	ft_cmd_setenv(char **av, char ***env)
 {
-	if (av[1] != NULL)
-	{
-		if (av[2] != NULL)
-			*env = ft_setenv(av[1], av[2], *env);
-		else
-			*env = ft_setenv(av[1], "", *env);
-	}
-	else
-		ft_putendl_fd("Error too few argument", 2);
-	return (1);
+  if (av[1] != NULL)
+  {
+    if (av[2] != NULL)
+      *env = ft_setenv(av[1], av[2], *env);
+    else
+      *env = ft_setenv(av[1], "", *env);
+  }
+  else
+    ft_putendl_fd("Error too few argument", 2);
+  return (1);
 }
 
 int	ft_cmd_cd(char **av, char **env)
 {
-	char	*path;
+  char	*path;
 
-	if (av[1] != NULL)
-	{
-		path = av[1];
-		if (chdir(path) < 0)
-		{
-			ft_putstr_fd("cd: No such file or directory: ", 2);
-			ft_putstr_fd(av[1], 2);
-		}
-	}
-	else
-	{
-		if ((path = ft_get_home(env)) != NULL && chdir(path) < 0)
-		{
-			ft_putstr_fd("cd: No such file or directory: ", 2);
-			ft_putstr_fd(av[1], 2);
-		}
-	}
-	env = ft_setenv("PWD", path, env);
-	return (1);
+  if (av[1] != NULL)
+  {
+    path = av[1];
+    if (chdir(path) < 0)
+    {
+      ft_putstr_fd("cd: No such file or directory: ", 2);
+      ft_putstr_fd(av[1], 2);
+    }
+  }
+  else
+  {
+    if ((path = ft_get_home(env)) != NULL && chdir(path) < 0)
+    {
+      ft_putstr_fd("cd: No such file or directory: ", 2);
+      ft_putstr_fd(av[1], 2);
+    }
+  }
+  env = ft_setenv("PWD", path, env);
+  return (1);
 }
 
 int	ft_cmd_env(char **av, char ***env_bis)
 {
-	if (av[1] != NULL)
-		ft_putendl_fd("Error env", 2);
-	else
-		ft_print_env(*env_bis);
-	return (1);
+  if (av[1] != NULL)
+    ft_putendl_fd("Error env", 2);
+  else
+    ft_print_env(*env_bis);
+  return (1);
 }
--- src/print.c
+++ src/print.c
@@ -14,70 +14,77 @@
 
 void ft_print_env(char **env_bis)
 {
-	int i;
+  int i;
 
-	if (env_bis == NULL)
-		return ;
-	i = 0;
-	while (env_bis[i])
-	{
-		ft_putendl(env_bis[i]);
-		i++;
-	}
+  if (env_bis == NULL)
+    return ;
+  i = 0;
+  while (env_bis[i])
+  {
+    ft_putendl(env_bis[i]);
+    i++;
+  }
 }
 
 void	ft_print_dollar_bis(char **av, char **env_bis)
 {
-	char			*tmp;
-	int				flag;
-	int				index;
-	int				i;
+  char			*tmp;
+  int				flag;
+  int				index;
+  int				i;
 
-	flag = 0;
-	index = 0;
-	i = 0;
-	while (av[++i])
-		if ((ft_strchr(av[i], '$')) != NULL && ft_no_digit(av[i]) == 0 && ft_strlen(av[i]) > 1)
-		{
-			tmp = ft_strcpy_cara(av[i]);
-			if ((index = ft_get_env(tmp, env_bis)) < 0)
-				flag = 1;
-		}
-	if (flag == 1)
-	{
-		ft_error_dollar(av, env_bis);
-		free(tmp);
-	}
-	else
-		ft_env_without(av, env_bis, &flag);
-	ft_putchar('\n');
+  flag = 0;
+  index = 0;
+  i = 0;
+  while (av[++i])
+    if ((ft_strchr(av[i], '$')) != NULL && ft_no_digit(av[i]) == 0 && ft_strlen(av[i]) > 1)
+    {
+      tmp = ft_strcpy_cara(av[i]);
+      if ((index = ft_get_env(tmp, env_bis)) < 0)
+	flag = 1;
+    }
+  if (flag == 1)
+  {
+    ft_error_dollar(av, env_bis);
+    free(tmp);
+  }
+  else
+    ft_env_without(av, env_bis, &flag);
+  ft_putchar('\n');
 }
 
 void	ft_print_dollar(char **av, char **env_bis)
 {
-	int	i;
-	int	j;
+  int	i;
+  int	j;
 
-	i = 0;
-	while (av[++i])
-	{
-		j = 0;
-		while (av[i][j])
-		{
-		/*	if (av[i][j] == '$' && (av[i][j + 1] == ' ' || av[i][j + 1] == '\0'))
-			{
-				ft_putchar(av[i][j]);
-				ft_putchar(' ');
-			}*/
-			if (av[i][j] == '$' && ft_isdigit(av[i][j + 1]) == 1)
-				;
-			else if ((av[i][j] == '$' && ft_isalpha(av[i][j + 1]) == 1) || av[i][j] != '$')
-			{
-				ft_print_dollar_bis(av, env_bis);
-				return ;
-			}
-			j++;
-		}
-	}
-	ft_putchar('\n');
+  i = 0;
+  while (av[++i])
+  {
+    j = 0;
+    while (av[i][j])
+    {
+      if (av[i][j] == '$' && ft_isdigit(av[i][j + 1]) == 1)
+	;
+      else if ((av[i][j] == '$' && ft_isalpha(av[i][j + 1]) == 1) || av[i][j] != '$')
+      {
+	ft_print_dollar_bis(av, env_bis);
+	return ;
+      }
+      j++;
+    }
+  }
+  ft_putchar('\n');
+}
+
+void	ft_print_acco(char **av, char **env_bis)
+{
+  int 	i;
+  char	*tmp;
+
+  i = 0;
+  while (av[i])
+  {
+
+  }
 }
